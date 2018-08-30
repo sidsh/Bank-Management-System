2876d7d3bb7f98be01c786f34584c06cee374908
Adnan abdullah <38535226+addddd123@users.noreply.github.com>
2018-08-30 07:45:02 +0000

    Update newaccount.h

--- newaccount.h
+++ newaccount.h
@@ -5,6 +5,7 @@ void craeteid()
 
 
 
-
+/* Enter name parentage dob and use fxn random(10000) to gentrate random id for exch customer 
+and data should be saved in files with that random id*/
     
 }
