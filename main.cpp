077f97f8616d068d28093a5dd5711f8626165541
Adnan abdullah <38535226+addddd123@users.noreply.github.com>
2018-08-30 13:05:16 +0530

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,34 @@
+//.......bank management main................
+
+
+#include<stdio.h>
+#include<iostream>
+using namespace std;
+
+
+int main()
+{
+int choice;
+cout<<"To Select any option press numbers that are before to options";
+cout<<"\n 1. create new account";
+cout<<"\n 2. get details of ur account";
+cout<<"\n 3.  update details of ur account";
+cout<<"\n 4. press to exit";
+cout<<"Press your choice\n";
+cin>>choice;
+switch(choice)
+{
+ case 1:
+      // createid();
+        break;
+case 2:
+       //searchid();
+      break;
+case 3: 
+       //modifydetails();
+case 4: exit (0);
+        break;
+default:  cout<<"Enter your valid choice";
+}
+}
+
