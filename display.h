040ad2ac263c15b17b7d8ee70608ec800019502f
Adnan abdullah <38535226+addddd123@users.noreply.github.com>
2018-09-01 05:24:07 +0000

    Add files via upload

---
+++ display.h
@@ -0,0 +1,30 @@
+#include<iostream>
+#include<fstream>
+#include<stdlib.h>
+#include<stdio.h>
+#include<string.h>
+#include<dos.h>
+using namespace std;
+int display()
+{
+	ifstream fin;
+	char phone[12];
+	cout<<"Enter your id"<<endl;
+	cin>>phone;
+	int a=strlen(phone);
+	if(a!=10)
+	{
+	cout<<"You have entered a wrong id "<<endl;
+	      exit(0);
+}
+	fin.open(phone);
+	int ch;
+	ch=fin.get();
+	while(!fin.eof())
+	{
+		cout<<ch;
+		ch=fin.get();
+	}
+	fin.close();
+	return 0;
+}
