251ef1c9530cd326276f4e750921b14458cefbf5
Saqib Imtiyaz <38892101+sidsh@users.noreply.github.com>
2018-09-01 16:28:42 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,44 +3,46 @@
 
 #include<stdio.h>
 #include<iostream>
+#include<conio.h>
 #include "newaccount.h"
 #include "addmoney.h"
 #include "display.h"
+#include<string>
 using namespace std;
 
 //***********Globalvaribles*********//
 long int ak;
 
 //**********prototypes***********
-long int id();
-void addmoneyy(long int);
-int display();
+int id();
+void addmoneyy();
+void display();
 
 int main()
 {
 int choice;
-cout<<"To Select any option press numbers that are before to options";
-cout<<"\n 1. create new account";
-cout<<"\n 2. get details of ur account";
-cout<<"\n 3.  update details of ur account";
-cout<<"\n 4. press to exit";
-cout<<"Press your choice\n";
+cout<<"....Welcome to our Bank and thanks for showing interest....\n........Lets get started........."<<endl;
+cout<<"  ..........MAIN MENU........... "<<endl;
+cout<<"To Select any option press numbers that are displayed before options";
+cout<<"\n 1. Create new account";
+cout<<"\n 2. Update details and balance of your account";
+cout<<"\n 3. Display details of your account";
+cout<<"\n 4. Press to exit";
+cout<<"Enter your choice\n";
 cin>>choice;
 switch(choice)
 {
 	
  case 1:
     ak =id();
-       
         break;
 case 2:
-       addmoneyy(ak);
-      
-	  break;
+       addmoneyy(ak); 
+	   break;
 case 3: 
        display();
 case 4: exit (0);
         break;
-default:  cout<<"Enter your valid choice";
+default:  cout<<"Enter a valid choice";
 }
 }
